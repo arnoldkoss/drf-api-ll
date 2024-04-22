faab0166e915cc7b741456b824fa7409f253710c
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-22 13:21:07 +0000

    add login and logout views, IsOwner permission and is_owner field to detectorist serializer

--- detectorists/serializers.py
+++ detectorists/serializers.py
@@ -4,10 +4,15 @@ from .models import Detectorist
 
 class DetectoristSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Detectorist
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'best_find'
+            'content', 'image', 'best_find', 'is_owner'
         ]
--- detectorists/views.py
+++ detectorists/views.py
@@ -4,32 +4,41 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Detectorist
 from .serializers import DetectoristSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class DetectoristList(APIView):
     def get(self, request):
         detectorists = Detectorist.objects.all()
-        serializer = DetectoristSerializer(detectorists, many=True)
+        serializer = DetectoristSerializer(
+            detectorists, many=True, context={'request': request})
         return Response(serializer.data)
 
 
 class DetectoristDetail(APIView):
     serializer_class = DetectoristSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
             detectorist = Detectorist.objects.get(pk=pk)
+            self.check_object_permissions(self.request, detectorist)
             return detectorist
         except Detectorist.DoesNotExist:
             raise Http404
     
     def get(self, request, pk):
         detectorist = self.get_object(pk)
-        serializer = DetectoristSerializer(detectorist)
+        serializer = DetectoristSerializer(
+            detectorist, context={'request': request}
+            )
         return Response(serializer.data)
     
     def put(self, request, pk):
         detectorist = self.get_object(pk)
-        serializer = DetectoristSerializer(detectorist, data=request.data)
+        serializer = DetectoristSerializer(
+            detectorist, data=request.data, context={'request': request}
+            )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
