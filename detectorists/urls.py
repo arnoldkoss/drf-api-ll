13452a813f8191bc2da90cbc91824991320d1f92
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-22 11:16:20 +0000

    add the detectorist list detail view

--- detectorists/urls.py
+++ detectorists/urls.py
@@ -3,4 +3,5 @@ from detectorists import views
 
 urlpatterns = [
     path('detectorists/', views.DetectoristList.as_view()),
+    path('detectorists/<int:pk>/', views.DetectoristDetail.as_view()),
 ]
--- detectorists/views.py
+++ detectorists/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Detectorist
@@ -9,3 +11,26 @@ class DetectoristList(APIView):
         detectorists = Detectorist.objects.all()
         serializer = DetectoristSerializer(detectorists, many=True)
         return Response(serializer.data)
+
+
+class DetectoristDetail(APIView):
+    serializer_class = DetectoristSerializer
+    def get_object(self, pk):
+        try:
+            detectorist = Detectorist.objects.get(pk=pk)
+            return detectorist
+        except Detectorist.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        detectorist = self.get_object(pk)
+        serializer = DetectoristSerializer(detectorist)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        detectorist = self.get_object(pk)
+        serializer = DetectoristSerializer(detectorist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
