900979b0a735a2fb341c80cdc37695825d253b71
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-24 10:53:59 +0000

    refactor the detectorists views with generics

--- detectorists/views.py
+++ detectorists/views.py
@@ -1,45 +1,21 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Detectorist
-from .serializers import DetectoristSerializer
+from rest_framework import generics
 from drf_api.permissions import IsOwnerOrReadOnly
+from .serializers import DetectoristSerializer
+from .models import Detectorist
 
 
-class DetectoristList(APIView):
-    def get(self, request):
-        detectorists = Detectorist.objects.all()
-        serializer = DetectoristSerializer(
-            detectorists, many=True, context={'request': request})
-        return Response(serializer.data)
-
-
-class DetectoristDetail(APIView):
+class DetectoristList(generics.ListAPIView):
+    """
+    List all detectorists. This endpoint provides a public view of detectorist records.
+    """
+    queryset = Detectorist.objects.all()
     serializer_class = DetectoristSerializer
-    permission_classes = [IsOwnerOrReadOnly]
 
-    def get_object(self, pk):
-        try:
-            detectorist = Detectorist.objects.get(pk=pk)
-            self.check_object_permissions(self.request, detectorist)
-            return detectorist
-        except Detectorist.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        detectorist = self.get_object(pk)
-        serializer = DetectoristSerializer(
-            detectorist, context={'request': request}
-            )
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        detectorist = self.get_object(pk)
-        serializer = DetectoristSerializer(
-            detectorist, data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DetectoristDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a detectorist's record. Only the owner has permission to update.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Detectorist.objects.all()
+    serializer_class = DetectoristSerializer
