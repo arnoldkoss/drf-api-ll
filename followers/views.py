30673a6b07d669e632a179b8030a638c3951bfe9
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-24 10:43:09 +0000

    create the views for the followers app

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    Provides a list of all Follower instances and allows for creating a new follow relationship.
+    Only authenticated users can create a new follow relationship. The list of followers
+    can be viewed by any user, whether authenticated or not.
+
+    The perform_create method is overridden to automatically set the 'owner' of a new
+    Follower instance to the currently authenticated user, ensuring that users can only
+    create follow relationships where they are the owner.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower
+    No Update view, as we either follow or unfollow users
+    Destroy a follower, i.e. unfollow someone if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
