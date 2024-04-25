b7019ce216616940085d5008342457c361405570
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-25 10:48:19 +0000

    add following id to detectorists serializers

--- detectorists/serializers.py
+++ detectorists/serializers.py
@@ -1,18 +1,32 @@
 from rest_framework import serializers
 from .models import Detectorist
+from followers.models import Follower
 
 
 class DetectoristSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            # print(following)
+            return following.id if following else None
+        return None
+    
 
     class Meta:
         model = Detectorist
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'best_find', 'is_owner'
+            'content', 'image', 'best_find', 'is_owner', 'following_id'
         ]
