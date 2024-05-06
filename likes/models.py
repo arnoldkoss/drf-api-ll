a73c6694545e9dcf4891438d6405e31f5f7859bc
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-06 12:50:33 +0000

    refactor the likes models

--- likes/models.py
+++ likes/models.py
@@ -1,28 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
-from comments.models import Comment
+
 
 
 class Like(models.Model):
     """
-    Like model, related to 'owner', 'post' and 'comment'.
+    Like model, related to 'owner', 'post' 
     'owner' is a User instance and 'post' is a Post instance.
     'unique_together' makes sure a user can't like the same post twice.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
         Post, related_name='likes', on_delete=models.CASCADE, null=True)
-    comment = models.ForeignKey(
-        Comment, related_name='likes', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_at']
-        unique_together = (('owner', 'post'), ('owner', 'comment'))
+        unique_together = ['owner', 'post']
 
     def __str__(self):
-        if self.post:
-            return f'{self.owner} {self.post}'
-        else:
-            return f'{self.owner} {self.comment}'
+        return f'{self.owner} {self.post}'
+        
