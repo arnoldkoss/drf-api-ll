2860168442b038484d06fbbe72ab7654f058a082
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-14 13:21:55 +0000

    rearange the models and serializers for the favorites

--- favorites/models.py
+++ favorites/models.py
@@ -1,30 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
-from detectorists.models import Detectorist
+
 
 class Favorite(models.Model):
     """
     Favorite model to allow users to save their 
-    favorite profiles.
-    Each favorite links a user to a post or another user's 
-    profile they have marked as favorite.
+    favorite posts.
+    Each favorite links a user to a post  they have marked as favorite.
     """
     owner = models.ForeignKey(
         User, related_name='favorites', on_delete=models.CASCADE)
     
-    detectorist = models.ForeignKey(
-        Detectorist, related_name='favorited_by_users', on_delete=models.CASCADE, null=True, blank=True)
+    post = models.ForeignKey(
+        Post, related_name='favorites', on_delete=models.CASCADE, null=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
 
     class Meta:
         ordering = ['-created_at']
-        unique_together = ('owner', 'detectorist')
+        unique_together = ('owner', 'post')
 
     
     def __str__(self):
-        return f'{self.owner} {self.detectorist}'
+        return f'{self.owner} {self.post}'
 
 
     
--- favorites/serializers.py
+++ favorites/serializers.py
@@ -7,19 +7,18 @@ class FavoriteSerializer(serializers.ModelSerializer):
     """
     Serializer for the Favorite model.
     Handles the creation of favorites while ensuring unique
-    constraints are respected between 'owner' and 'post',
-    and 'owner' and 'detectorist'.
+    constraints are respected between 'owner' and 'post'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Favorite
-        fields = ['id', 'owner', 'detectorist', 'created_at']
+        fields = ['id', 'owner', 'post', 'created_at']
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({
-                'detail': 'possible duplicate'
+                'detail': 'This is already in your wishlist.'
             })
