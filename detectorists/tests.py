7b0d8974105823b42b38e6e6989916f3d2512e7f
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-15 11:07:58 +0000

    create test for detectorist

--- detectorists/tests.py
+++ detectorists/tests.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
+from .models import Detectorist
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserDetectoristCountTest(TestCase):
+    # make sure that the user is always extended with the detectorist model
+    def test_user_detectorist_count(self):
+        user_count = User.objects.count()
+        detectorist_count = Detectorist.objects.count()
+        self.assertEqual(user_count, detectorist_count)
